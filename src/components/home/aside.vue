30aa8647677aa0c5590130844bd5897c2b04585f
李帅 <405904262@qq.com>
2019-12-21 11:26:24 +0800

    一级路由完毕，二级路由开始

--- src/components/home/aside.vue
+++ src/components/home/aside.vue
@@ -1,8 +1,32 @@
 <template>
-  <el-menu>
-      <el-menu-item>首页</el-menu-item>
-      <el-menu-item>账户信息</el-menu-item>
+<div class="asided">
+  <div class="logo">
+    <img src="./../../assets/img/logo_admin.png" alt="">
+  </div>
+  <!-- :router="ture" 完整写法 -->
+  <el-menu router background-color="#323745" text-color="#adafb5">
+      <el-menu-item>
+        <span slot="title" index="/home">首页</span>
+      </el-menu-item>
+      <el-submenu index="1">
+        <span slot="title">内容管理</span>
+        <el-menu-item index="/home/publish">发布文章</el-menu-item>
+        <el-menu-item index="/home/articles">文章列表</el-menu-item>
+        <el-menu-item index="/home/articles">评论管理</el-menu-item>
+        <el-menu-item index="/home/material">素材管理</el-menu-item>
+      </el-submenu>
+      <el-submenu index="2">
+        <span slot="title">粉丝管理</span>
+        <el-menu-item index="/home/data">图文数据</el-menu-item>
+        <el-menu-item index="/home/fansinfo">粉丝概况</el-menu-item>
+        <el-menu-item index="/home/fanspic">粉丝画像</el-menu-item>
+        <el-menu-item index="/home/fanslist">粉丝列表</el-menu-item>
+      </el-submenu>
+      <el-menu-item>
+        <span slot="title" index="/home/userinfo">账户信息</span>
+      </el-menu-item>
   </el-menu>
+</div>
 </template>
 
 <script>
@@ -11,5 +35,21 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.asided{
+  background: #323745;
+  width: 230px;
+  height: 100vh;
+  .logo{
+    text-align: center;
+    background: #2e2f32;
+    padding: 10px 0;
+    img{
+      height: 35px;
+    }
+  };
+  .el-menu{
+    border-right:none
+  }
+}
 </style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,9 +1,34 @@
 <template>
   <el-container>
-    <el-aside><asided></asided></el-aside>
+    <el-aside style="width:230px"><asided></asided></el-aside>
     <el-container>
-      <el-header>头</el-header>
-      <el-main>主要区域</el-main>
+      <el-header>
+        <el-row type="flex" align="middle">
+          <el-col class="left" :span="12">
+            <i class='el-icon-s-fold'></i>
+            <span>江苏传智播客教育科技股份有限公司</span>
+          </el-col>
+          <el-col class="right" :span="12">
+            <el-row type="flex" justify="end" align="middle">
+              <img :src="userInfo.photo ? userInfo.photo : defauiltImg" alt="">
+              <el-dropdown @command="clickMenu">
+                <span class="el-dropdown-link">
+                  {{userInfo.name}}<i class="el-icon-arrow-down el-icon--right"></i>
+                </span>
+                <el-dropdown-menu slot="dropdown">
+                  <el-dropdown-item command="info">个人信息</el-dropdown-item>
+                  <el-dropdown-item command="git">git</el-dropdown-item>
+                  <el-dropdown-item command="lgout">退出</el-dropdown-item>
+                  </el-dropdown-menu>
+              </el-dropdown>
+            </el-row>
+          </el-col>
+        </el-row>
+      </el-header>
+      <el-main>
+        <!-- 二级路由 -->
+        <router-view></router-view>
+      </el-main>
     </el-container>
   </el-container>
 </template>
@@ -11,13 +36,55 @@
 <script>
 import aside from '../../components/home/aside'
 export default {
+  data () {
+    return {
+      userInfo: {},
+      defauiltImg: require('./../../assets/img/avatar.jpg')
+    }
+  },
   components: {
     // 标签冲突
     'asided': aside
+  },
+  methods: {
+    clickMenu (command) {
+      if (command === 'info') {
+
+      } else if (command === 'git') {
+
+      } else {
+        localStorage.removeItem('user-token')
+        this.$router.push('/login')
+      }
+    }
+  },
+  created () {
+    let token = localStorage.getItem('user-token')
+    this.$axios({
+      url: '/user/profile',
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }).then(res => {
+      this.userInfo = res.data.data
+      console.log(res)
+    })
   }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.left{
+  span{
+    margin-left: 5px;
+  }
+}
+.right{
+  img{
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    margin-right: 8px;
+  }
+}
 </style>
