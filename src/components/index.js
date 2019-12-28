abcbac3e415d160de0ff3f490498e736d6575d1c
李帅 <405904262@qq.com>
2019-12-28 09:38:54 +0800

    添加富文本编辑器

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,15 @@
 import aside from '../components/home/aside.vue'
 import layheader from '../components/home/header.vue'
 import breadCrumb from '../common/bread-crumb.vue'
-
+import { quillEditor } from 'vue-quill-editor'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 export default {
   install (Vue) {
-    Vue.component('asided', aside)
-    Vue.component('headers', layheader)
-    Vue.component('bread-crumb', breadCrumb)
+    Vue.component('asided', aside) // 侧边栏注册
+    Vue.component('headers', layheader) // 头部注册
+    Vue.component('bread-crumb', breadCrumb) // 面包屑组件注册
+    Vue.component('quill-editor', quillEditor)
   }
 }
